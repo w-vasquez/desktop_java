96a55612682cab9eba619e27ed364e49b54a6756
w-vasquez <2740882018@mai.utec.edu.sv>
2020-05-25 09:06:04 -0600

    pendiente modulo ordenanza

--- src/Ordenanzas/frmMenuOrdenanzas.java
+++ src/Ordenanzas/frmMenuOrdenanzas.java
@@ -35,9 +35,6 @@ public class frmMenuOrdenanzas extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         jPanel3 = new javax.swing.JPanel();
-        btnprogramaciones = new rsbuttom.RSButtonMetro();
-        btnhistorial = new rsbuttom.RSButtonMetro();
-        btncerrrar = new rsbuttom.RSButtonMetro();
         escritorio = new javax.swing.JDesktopPane();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -74,54 +71,6 @@ public class frmMenuOrdenanzas extends javax.swing.JFrame {
 
         jPanel3.setBackground(new java.awt.Color(38, 38, 38));
         jPanel3.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        btnprogramaciones.setBackground(new java.awt.Color(38, 38, 38));
-        btnprogramaciones.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Programaciones2.png"))); // NOI18N
-        btnprogramaciones.setText("Programaciones");
-        btnprogramaciones.setColorHover(new java.awt.Color(156, 127, 0));
-        btnprogramaciones.setColorNormal(new java.awt.Color(38, 38, 38));
-        btnprogramaciones.setColorPressed(new java.awt.Color(224, 182, 0));
-        btnprogramaciones.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
-        btnprogramaciones.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
-        btnprogramaciones.setIconTextGap(10);
-        btnprogramaciones.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnprogramacionesActionPerformed(evt);
-            }
-        });
-        jPanel3.add(btnprogramaciones, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 220, 140));
-
-        btnhistorial.setBackground(new java.awt.Color(38, 38, 38));
-        btnhistorial.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/Historial.png"))); // NOI18N
-        btnhistorial.setText("HISTORIAL");
-        btnhistorial.setColorHover(new java.awt.Color(5, 134, 154));
-        btnhistorial.setColorNormal(new java.awt.Color(38, 38, 38));
-        btnhistorial.setColorPressed(new java.awt.Color(3, 94, 107));
-        btnhistorial.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
-        btnhistorial.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
-        btnhistorial.setIconTextGap(10);
-        btnhistorial.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnhistorialActionPerformed(evt);
-            }
-        });
-        jPanel3.add(btnhistorial, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 140, 220, 138));
-
-        btncerrrar.setBackground(new java.awt.Color(38, 38, 38));
-        btncerrrar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/cerrar2.png"))); // NOI18N
-        btncerrrar.setText("CERRAR SESIÓN");
-        btncerrrar.setColorHover(new java.awt.Color(215, 79, 42));
-        btncerrrar.setColorNormal(new java.awt.Color(38, 38, 38));
-        btncerrrar.setColorPressed(new java.awt.Color(150, 55, 29));
-        btncerrrar.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
-        btncerrrar.setIconTextGap(10);
-        btncerrrar.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btncerrrarActionPerformed(evt);
-            }
-        });
-        jPanel3.add(btncerrrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 278, 220, 130));
-
         jPanel1.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 70, 220, 690));
 
         javax.swing.GroupLayout escritorioLayout = new javax.swing.GroupLayout(escritorio);
@@ -212,9 +161,6 @@ public class frmMenuOrdenanzas extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private rsbuttom.RSButtonMetro btncerrrar;
-    private rsbuttom.RSButtonMetro btnhistorial;
-    private rsbuttom.RSButtonMetro btnprogramaciones;
     private javax.swing.JDesktopPane escritorio;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
